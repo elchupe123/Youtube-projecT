737d2ab80bcca199d34ae7fedc2c458d1e8ec760
elchupe123 <elchupeyt123@gmail.com>
2022-11-01 23:45:53 -0300

    Second commit (Just somes renames)

--- downloader.py
+++ downloader.py
@@ -29,15 +29,15 @@ title = screen.title("Descargador de Videos")
 canvas = Canvas(screen, width=500, height=500)
 canvas.pack()
 
-logo_img = PhotoImage(file='yt.png')
-logo_img = logo_img.subsample(8,8)
-canvas.create_image(250, 80, image=logo_img)
+youtube_logo_img = PhotoImage(file='youtube_logo.png')
+youtube_logo_img = youtube_logo_img.subsample(8, 8)
+canvas.create_image(250, 80, image=youtube_logo_img)
 
-dw_img = PhotoImage(file='dw.png')
-dw_img = dw_img.subsample(2,2)
+download_img = PhotoImage(file='download_img.png')
+download_img = download_img.subsample(2, 2)
 
-path = PhotoImage(file='path.png')
-path = path.subsample(7,7)
+file_explorer_img = PhotoImage(file='file_explorer_img.png')
+file_explorer_img = file_explorer_img.subsample(7, 7)
 
 
 
@@ -46,7 +46,7 @@ link_field = Entry(screen, width=50)
 link_label = Label(screen, text="Ingresa el link del video: ", font=("Arial", 15))
 
 path_label = Label(screen, text="Selecciona la ruta de descarga: ", font=("Arial", 15))
-select_btn = Button(screen, image=path, command=select_path)
+select_btn = Button(screen, image=file_explorer_img, command=select_path)
 
 canvas.create_window(230, 280, window=path_label)
 canvas.create_window(400, 280, window=select_btn)
@@ -58,7 +58,7 @@ canvas.create_window(250, 170, window=link_label)
 canvas.create_window(250, 200, window=link_field)
 
 
-download_btn = Button(screen, image=dw_img, command=download_file)
+download_btn = Button(screen, image=download_img, command=download_file)
 canvas.create_window(250, 390, window=download_btn)
 
 
